75aa8754c4b0b10dce7af98149f73288b9125d06
aqshah <syyed.aq@gmail.com>
2023-06-19 17:31:51 +0500

    invoices functions

--- public/home.php
+++ public/home.php
@@ -21,6 +21,8 @@
     </div>
 
 
+    <?php include("../includes/views/home_buttons_view.php"); ?>
+
     <?php include("../includes/views/sales_dashboard_1.php"); ?>
 
     <?php include("../includes/views/dispatch_stats_1.php"); ?>
@@ -32,46 +34,6 @@
     <?php include("../includes/views/dispatch_dashboard_1.php"); ?>
 
     <?php include("../includes/views/revenue_dashboard_1.php"); ?>
-
-    <div class="row">
-        <?php if ($user['permission']==1){ ?>
-        <div class="col-12 col-md-3 my-2">
-            <div class="d-grid gap-3">
-                <button type="button" class="primary-button">Invoice Management</button>
-            </div>
-        </div>
-        <?php } ?>
-        <?php if ($user['permission']==1 || $user['permission']==5){ ?>
-        <div class="col-12 col-md-3 my-2">
-            <div class="d-grid gap-3">
-                <button type="button" class="primary-button" onclick="location.href='list_carriers'">Carriers</button>
-            </div>
-        </div>
-        <div class="col-12 col-md-3 my-2">
-            <div class="d-grid gap-3">
-                <button type="button" class="primary-button" onclick="location.href='list_dispatching'">Available for
-                    Dispatch</button>
-            </div>
-        </div>
-        <div class="col-12 col-md-3 my-2">
-            <div class="d-grid gap-3">
-                <button type="button" class="primary-button" onclick="location.href='list_unavailable'">Unavailable
-                    Carriers</button>
-            </div>
-        </div>
-
-        <?php } ?>
-        <?php if ($user['permission']==1 || $user['permission']==10){ ?>
-        <div class="col-12 col-md-3 my-2">
-            <div class="d-grid gap-3">
-                <button type="button" class="primary-button" onclick="location.href='carrier_create'">New
-                    Carrier</button>
-            </div>
-        </div>
-        <?php } ?>
-
-
-    </div>
 
     <?php include("../includes/views/sales_agent_performance_1.php"); ?>
 
