2156b0d150cbbafd363eca17f854cc20ae6bf87c
AQ-Shah <syyed.aq@gmail.com>
2023-11-07 21:38:39 +0500

    Bug Fixed

--- includes/functions/staff_functions.php
+++ includes/functions/staff_functions.php
@@ -184,7 +184,7 @@ function find_users_by_department($department_id, $start, $end){
         $query  = "SELECT * ";
         $query .= "FROM users ";
         $query .= "WHERE department_id = {$safe_department_id} ";
-        $query .= "ORDER BY status ASC, designation DESC, full_name ASC ";
+        $query .= "ORDER BY status ASC, team_id ASC, designation DESC, full_name ASC ";
         $query .= "LIMIT {$start},{$end} ";
         $users_set = mysqli_query($connection, $query);
         confirm_query($users_set);
