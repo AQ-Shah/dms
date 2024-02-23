67e5acc56ae6bfe254d8ce9fb2bb37a7c6c13789
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-02-24 02:18:11 +0500

    adding index.php

---
+++ includes/config.php
@@ -0,0 +1,26 @@
+<?php
+
+//API Keys
+define("API_KEY", "your-api-key-here");
+define("API_SECRET", "your-api-secret-here");
+
+//ENCRYPTION_KEY
+define("ENCRYPTION_KEY", "24.7Dispatch@!");
+define("ID_VECTOR_KEY", "2479687521963084");
+define("PERMISSION_VECTOR_KEY", "2478492057129376");
+
+//Domain URL Info
+define("BASE_URL", "https://24.7dispatch.com");
+define("TIMEZONE", "America/New_York");
+
+//Company Info
+define("COMPANY_NAME", "7 Dispatch LLC");
+define("COMPANY_NUMBER", "302-655-2211");
+define("COMPANY_ADDRESS", "30 N Gould St, Sheridan, WY 82801.");
+
+//DB Info
+define("DB_SERVER", "localhost");
+define("DB_USER", "hackmast_24_7dispatch_wa");
+define("DB_PASS", "24.7DispatchWA@!");
+define("DB_NAME", "hackmast_24_7dispatch");
+?>
---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+header("Refresh: 0; URL=home.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Redirecting...</title>
+</head>
+<body>
+    <!-- Optional: You can add a message for users who don't support redirection -->
+    <p>If you are not redirected automatically, <a href="home.php">click here</a>.</p>
+</body>
+</html>
