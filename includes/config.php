4bd7411dca7827f715e8a4f70c8d5bb8f587bce2
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-03-07 02:51:06 +0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,22 +5,22 @@ define("API_KEY", "your-api-key-here");
 define("API_SECRET", "your-api-secret-here");
 
 //ENCRYPTION_KEY
-define("ENCRYPTION_KEY", "24.7Dispatch@!");
-define("ID_VECTOR_KEY", "2479687521963084");
-define("PERMISSION_VECTOR_KEY", "2478492057129376");
+define("ENCRYPTION_KEY", "yourkey");
+define("ID_VECTOR_KEY", "yourkey");
+define("PERMISSION_VECTOR_KEY", "yourkey");
 
 //Domain URL Info
-define("BASE_URL", "https://24.7dispatch.com");
+define("BASE_URL", "yoururl");
 define("TIMEZONE", "America/New_York");
 
 //Company Info
-define("COMPANY_NAME", "7 Dispatch LLC");
-define("COMPANY_NUMBER", "302-655-2211");
-define("COMPANY_ADDRESS", "30 N Gould St, Sheridan, WY 82801.");
+define("COMPANY_NAME", "yourcompany");
+define("COMPANY_NUMBER", "your numner");
+define("COMPANY_ADDRESS", "your address");
 
 //DB Info
 define("DB_SERVER", "localhost");
-define("DB_USER", "hackmast_24_7dispatch_wa");
-define("DB_PASS", "24.7DispatchWA@!");
-define("DB_NAME", "hackmast_24_7dispatch");
+define("DB_USER", "dbuser");
+define("DB_PASS", "pass");
+define("DB_NAME", "name");
 ?>
