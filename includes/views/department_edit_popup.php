3cbd2a5a2e0d6d5d0263223f39e20b1bce6c32b7
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-04-18 06:45:46 +0500

    testing attemp 3

--- includes/views/department_edit_popup.php
+++ includes/views/department_edit_popup.php
@@ -1,42 +1,72 @@
-<div id="department-edit-popup" class="popup">
+<div id="department-edit-popup" class="popup" style="display:none;">
     <div class="popup-content">
         <h2>Edit Unit</h2>
         <form class="department-edit-popup-form popup-form" action="" method="post">
             <label for="name">Name:</label>
             <input type="text" id="name" name="name" value=""><br>
             <label for="function-type">Unit Function:</label>
-            <select name="function-type" class="form-control w-100">
+            <select id="function-type" name="function-type" class="form-control w-100">
                 <option value="5">Dispatching</option>
                 <option value="10">Clients Acquisition</option>
             </select>
             <input type="hidden" id="department_id" name="department_id" value="">
-            <input type="hidden" name="prev_url" value="<?php echo $_SERVER['REQUEST_URI']; ?>">
-            <button type="submit" name="submit" onclick="hideDepartmentEditPopup()">Edit</button>
+            <input type="hidden" name="prev_url" value="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']); ?>">
+            <button type="submit" name="submit" onclick="event.preventDefault(); submitEditForm();">Edit</button>
             <button type="button" onclick="hideDepartmentEditPopup()">Cancel</button>
         </form>
     </div>
 </div>
 
+
 <script>
-//for dispatch popup
 
+// For dispatch popup
 function showDepartmentEditPopup(recordId, recordName, functionType) {
-    // Populate the form fields with default values
-    document.getElementById("department_id").value = recordId;
-    document.getElementById("name").value = recordName;
-    document.getElementById("function-type").value = functionType;
+    const popup = document.getElementById("department-edit-popup");
+    const departmentIdInput = document.getElementById("department_id");
+    const nameInput = document.getElementById("name");
+    const functionTypeSelect = document.getElementById("function-type");
 
-    var formAction = "department_edit?id=" + recordId;
+    if (popup && departmentIdInput && nameInput && functionTypeSelect) {
+        departmentIdInput.value = recordId;
+        nameInput.value = recordName;
+        functionTypeSelect.value = functionType;
 
-    // set the form action to the constructed URL
-    document.querySelector(".department-edit-popup-form").action = formAction;
-    // Show the popup
-    var popup = document.getElementById("department-edit-popup");
-    popup.style.display = "flex";
+        // Ensure the appropriate option is selected
+        for (let option of functionTypeSelect.options) {
+            if (option.value === functionType.toString()) {
+                option.selected = true;
+                break;
+            }
+        }
+
+        let formAction = "department_edit?id=" + recordId;
+        document.querySelector(".department-edit-popup-form").action = formAction;
+
+        // Show the popup by setting display to flex
+        popup.style.display = "flex";
+    } else {
+        console.error("Element not found!");
+    }
 }
 
 function hideDepartmentEditPopup() {
-    var popup = document.getElementById("department-edit-popup");
-    popup.style.display = "none";
+    const popup = document.getElementById("department-edit-popup");
+    if (popup) {
+        // Hide the popup by setting display to none
+        popup.style.display = "none";
+    } else {
+        console.error("Element not found!");
+    }
 }
+
+function submitEditForm() {
+    const form = document.querySelector(".department-edit-popup-form");
+    if (form) {
+        form.submit();
+    } else {
+        console.error("Form not found!");
+    }
+}
+
 </script>
