8a29e595bfa7f51b683ba2add9ee12d6aae23578
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-04-20 02:30:00 +0500

    test

--- includes/api/login.php
+++ includes/api/login.php
@@ -21,12 +21,12 @@ if (isset($_POST['submit'])) {
 
             $user_encrypted_id = openssl_encrypt($found_user["id"], "AES-256-CBC", $encryption_key, 0, $id_vector_key);
             $user_encrypted_permission = openssl_encrypt($found_user["permission"], "AES-256-CBC", $encryption_key, 0, $permission_vector_key);
-        
+            echo "heree";
             $encrypted_id = encrypt_id($found_user["id"]);
-            $encrypted_permission = encrypt_permission($found_user["permission"]);
+           // $encrypted_permission = encrypt_permission($found_user["permission"]);
         
             setcookie("idENC", $encrypted_id, time() + 28800, "/", "", false, false);
-            setcookie("permissionENC", $encrypted_permission, time() + 28800, "/", "", false, false);
+          //  setcookie("permissionENC", $encrypted_permission, time() + 28800, "/", "", false, false);
 
             setcookie("id", $user_encrypted_id, time() + 28800, "/", "", false, false);
             setcookie("permission", $user_encrypted_permission, time() + 28800, "/", "", false, false);
