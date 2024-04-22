1a88185e98ae685a2c0d6953ee69a8afb4258235
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-04-23 03:10:14 +0500

    Dispatch Views for Company B1.2.5

--- includes/data/dispatch_graph_by_agents.php
+++ includes/data/dispatch_graph_by_agents.php
@@ -1,6 +1,6 @@
 <?php
 
-$record_set = find_all_active_dispatchers();
+$record_set = find_all_dispatcher();
 
 ?>
 <script>
@@ -11,7 +11,7 @@ var dispatchGraphByAgentsCreation = new Chart(dispatchGraphByAgents, {
         labels: [
             <?php while($record = mysqli_fetch_assoc($record_set)) {echo "'".$record['full_name']."',"; }?>
         ],
-        <?php $record_set = find_all_active_dispatchers(); ?>
+        <?php $record_set = find_all_dispatcher(); ?>
         datasets: [{
             label: 'Dispatch Agents',
             data: [
