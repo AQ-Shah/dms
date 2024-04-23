5e7e466a628452e0054bb40ebae8b5eadd170ee7
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-04-23 08:26:11 +0500

    minor bug fixed

--- includes/api/carrier_create_query.php
+++ includes/api/carrier_create_query.php
@@ -6,13 +6,14 @@
 
     $creator_id = $user['id'];
     $sales_team_id = $user['team_id'];
+    $company_id = $user['company_id'];
     include("validators/carrier_post_checker.php");
 
     if (empty($errors)) {
       // Perform the insertion query
 
-      $sql = "INSERT INTO carrier_form (creator_id, sales_team_id,dot, mc, b_name, dba, b_address, o_name, b_number, b_email, b_type, tax_id, mc_validity, insurance_name, insurance_number, insurance_street, insurance_state, insurance_email, cgl_no, cgl_limit, cgl_expiration, aml_no, aml_limit, aml_expiration, mtc_no, mtc_limit, mtc_expiration, tic_no, tic_limit, tic_expiration, factoring_name, factoring_number, factoring_street, factoring_state, factoring_email, percentage, weekly_fixed)
-      VALUES ('$creator_id','$sales_team_id', '$dot', '$mc', '$b_name', '$dba','$b_address', '$o_name', '$b_number', '$b_email', '$b_type', '$tax_id','$mc_validity', '$insurance_name', '$insurance_number', '$insurance_street', '$insurance_state', '$insurance_email', '$cgl_no', '$cgl_limit', '$cgl_expiration', '$aml_no', '$aml_limit', '$aml_expiration', '$mtc_no', '$mtc_limit', '$mtc_expiration', '$tic_no', '$tic_limit', '$tic_expiration', '$factoring_name', '$factoring_number', '$factoring_street', '$factoring_state', '$factoring_email', '$percentage', '$weekly_fixed')";
+      $sql = "INSERT INTO carrier_form (company_id, creator_id, sales_team_id,dot, mc, b_name, dba, b_address, o_name, b_number, b_email, b_type, tax_id, mc_validity, insurance_name, insurance_number, insurance_street, insurance_state, insurance_email, cgl_no, cgl_limit, cgl_expiration, aml_no, aml_limit, aml_expiration, mtc_no, mtc_limit, mtc_expiration, tic_no, tic_limit, tic_expiration, factoring_name, factoring_number, factoring_street, factoring_state, factoring_email, percentage, weekly_fixed)
+      VALUES ('$company_id','$creator_id','$sales_team_id', '$dot', '$mc', '$b_name', '$dba','$b_address', '$o_name', '$b_number', '$b_email', '$b_type', '$tax_id','$mc_validity', '$insurance_name', '$insurance_number', '$insurance_street', '$insurance_state', '$insurance_email', '$cgl_no', '$cgl_limit', '$cgl_expiration', '$aml_no', '$aml_limit', '$aml_expiration', '$mtc_no', '$mtc_limit', '$mtc_expiration', '$tic_no', '$tic_limit', '$tic_expiration', '$factoring_name', '$factoring_number', '$factoring_street', '$factoring_state', '$factoring_email', '$percentage', '$weekly_fixed')";
       $result = mysqli_query($connection, $sql);
 
       if ($result) {
--- includes/api/carrier_dispatch_query.php
+++ includes/api/carrier_dispatch_query.php
@@ -35,8 +35,8 @@
 
     if (empty($errors)) { 
         
-        $query = "INSERT INTO dispatch_list (carrier_id, truck_id, dispatcher_id, dispatch_team_id, current_location, new_location, rate, commission, delivery_time, dispatch_time) ";
-        $query .= "VALUES ('" . $carrier["id"] . "', '" . $truck_id ."','" . $dispatcher_id ."', '" . $dispatch_team_id ."', '" . $current_location . "', '$new_location', '$rate', '$commission','$delivery_time','$pickup_datetime'); ";
+        $query = "INSERT INTO dispatch_list (company_id, carrier_id, truck_id, dispatcher_id, dispatch_team_id, current_location, new_location, rate, commission, delivery_time, dispatch_time) ";
+        $query .= "VALUES ('" . $carrier["company_id"] . "','" . $carrier["id"] . "', '" . $truck_id ."','" . $dispatcher_id ."', '" . $dispatch_team_id ."', '" . $current_location . "', '$new_location', '$rate', '$commission','$delivery_time','$pickup_datetime'); ";
         $query .= "UPDATE trucks_info SET truck_load_status = 2, current_location = '$new_location' WHERE id = $truck_id LIMIT 1; ";
         if ($carrier['sale_activation_dispatch_id'] == 0) 
         $query .= "UPDATE carrier_form SET sale_active = 1,sale_activation_date = CURRENT_TIMESTAMP, sale_activation_dispatch_id = LAST_INSERT_ID() WHERE id = {$carrier["id"]} LIMIT 1";
