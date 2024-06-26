ab5ab1dd78f034dd1541a4c98212cb238d6f8bcf
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-06-27 00:07:37 +0500

    dispatch board 1.3.6

--- public/dispatch_board.php
+++ public/dispatch_board.php
@@ -37,9 +37,7 @@
                         <?php while($record = mysqli_fetch_assoc($available_trucks)) { ?>
                         <?php if ($record["truck_load_status"] == 1) { ?>
                         <div class="kanban-item" draggable="true" data-id="<?php echo $record['id']; ?>">
-                            <p><?php 
-                                $carrier = find_carrier_form_by_id($record["carrier_id"]); 
-                                echo htmlentities($carrier['b_name']).' '.htmlentities($record["d_name"])?></p>
+                            <p><?php echo htmlentities(find_carrier_form_by_id($record["carrier_id"])['b_name']) . ' - ' . htmlentities($record["d_name"]); ?></p>
                         </div>
                         <?php } ?>
                         <?php } ?>
@@ -53,7 +51,7 @@
                         <?php while($record = mysqli_fetch_assoc($onload_trucks)) { ?>
                         <?php if ($record["truck_load_status"] == 2) { ?>
                         <div class="kanban-item" draggable="true" data-id="<?php echo $record['id']; ?>">
-                         
+                            <p><?php echo htmlentities(find_carrier_form_by_id($record["carrier_id"])['b_name']) . ' - ' . htmlentities($record["d_name"]); ?></p>
                         </div>
                         <?php } ?>
                         <?php } ?>
@@ -67,7 +65,7 @@
                         <?php while($record = mysqli_fetch_assoc($unavailable_trucks)) { ?>
                         <?php if ($record["truck_load_status"] == 3) { ?>
                         <div class="kanban-item" draggable="true" data-id="<?php echo $record['id']; ?>">
-                            
+                            <p><?php echo htmlentities(find_carrier_form_by_id($record["carrier_id"])['b_name']) . ' - ' . htmlentities($record["d_name"]); ?></p>  
                         </div>
                         <?php } ?>
                         <?php } ?>
