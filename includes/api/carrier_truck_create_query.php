49e110bd94874fcd86b3b464c6fb276e90430fb5
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-09-27 01:06:21 +0500

    dispatch board 1.3.10.1 : adding driver

--- includes/api/carrier_truck_create_query.php
+++ includes/api/carrier_truck_create_query.php
@@ -5,14 +5,15 @@
     // validations
     $required_fields = array("truck_type", "d_name", "d_number", "trucks-carrier-id");
     validate_presences($required_fields);
+    $company_id = $user['company_id'];
 
     include("validators/carrier_truck_post_checker.php");
 
     if (empty($errors)) {
       // Perform the insertion query
 
-      $sql = "INSERT INTO trucks_info (carrier_id, truck_type, d_name, d_number, t_length, t_weight, truck_no, trailer_no, hazmat, twic, sida, atp)
-      VALUES ('$carrier_id','$truck_type', '$d_name', '$d_number', '$t_length', '$t_weight','$truck_no', '$trailer_no', '$hazmat', '$twic', '$sida', '$atp')";
+      $sql = "INSERT INTO trucks_info (company_id, carrier_id, truck_type, d_name, d_number, t_length, t_weight, truck_no, trailer_no, hazmat, twic, sida, atp)
+      VALUES ('$company_id',''$carrier_id','$truck_type', '$d_name', '$d_number', '$t_length', '$t_weight','$truck_no', '$trailer_no', '$hazmat', '$twic', '$sida', '$atp')";
       $result = mysqli_query($connection, $sql);
       if ($result) {
         $_SESSION["message"] = "New Carrier Truck Added.";
