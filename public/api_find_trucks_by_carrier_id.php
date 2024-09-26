9497a3245e9fd25085f78b537afbcb55782b491c
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-09-27 04:23:52 +0500

    dispatch board 1.3.10.17 : edit truck

--- public/api_find_trucks_by_carrier_id.php
+++ public/api_find_trucks_by_carrier_id.php
@@ -4,15 +4,17 @@
 	
     confirm_access($current_page);
 
-function find_trucks($id) {
+function find_trucks($id, $user) {
     global $connection;
 
     $safe_id = mysqli_real_escape_string($connection, $id);
+    $safe_company_id = $user["company_id"]; 
 
     $query = "
         SELECT *
         FROM trucks_info
         WHERE carrier_id = '{$safe_id}'
+        AND company_id = '{$safe_company_id}' 
     ";
 
     $data_set = mysqli_query($connection, $query);
@@ -30,7 +32,7 @@ function find_trucks($id) {
 
 $id = $_GET['id'];
 if (find_carrier_form_by_id($id)) {
-    $trucks = find_trucks($id);
+    $trucks = find_trucks($id, $user);
     echo $trucks;
 }
 
