8c6853d35477d4ac583c808951cd1066f5b5c0a5
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-09-27 00:47:14 +0500

    dispatch board 1.3.10.9 : add truck view

--- includes/views/truck_form.php
+++ includes/views/truck_form.php
@@ -1,116 +1,90 @@
-<!-- Drivers Info -->
-<div class="row panel-content-primary card">
-
-
-
-
-</div>
-<!-- Truck and Driver Info -->
-<div class="row panel-content-primary card">
+<div class="row card">
 
     <div class="col-12 panel-title mb-3">
-        <h2>Truck & Drivers Info</h2>
+        <h2>Truck & Driver Info</h2>
     </div>
 
-    <div class="col-12 col-lg-6 panel-content-secondary">
-        <div class="col-12 col-lg-6">Driver Name : </div>
-        <div class="col-12 col-lg-6"><input type="text" class="form-control w-100" name="d_name"
-                value="<?php if (isset($d_name)){echo $d_name;}  ?>"></div>
-    </div>
-    <div class="col-12 col-lg-6 panel-content-secondary">
-        <div class="col-12 col-lg-6">Driver Number : </div>
-        <div class="col-12 col-lg-6"><input type="tel" pattern="[0-9]{10}" minlength="10" maxlength="10"
-                class="form-control w-100" name="d_number" value="<?php if (isset($d_number)){echo $d_number;}  ?>">
-        </div>
+    <div class="col-lg-6 panel-content-secondary">
+        <label for="d_name">Driver Name:</label>
+        <input type="text" id="d_name" class="form-control" name="d_name" 
+               value="<?php if (isset($d_name)){echo $d_name;} ?>">
     </div>
 
-    <div class="col-12 col-lg-6 panel-content-secondary">
-        <div class="col-12 col-lg-6">Truck Type : *</div>
-        <div class="col-12 col-lg-6">
-            <select name="truck_type" class="form-control w-100" required>
-                <option value="">Select truck type</option>
-                <option value="Box Truck"
-                    <?php if (isset($truck_type) && $truck_type == "Box Truck") echo 'selected'; ?>>Box Truck
-                </option>
-                <option value="Dry Van" <?php if (isset($truck_type) && $truck_type == "Dry Van") echo 'selected'; ?>>
-                    Dry Van</option>
-                <option value="Reefer" <?php if (isset($truck_type) && $truck_type == "Reefer") echo 'selected'; ?>>
-                    Reefer</option>
-                <option value="Power Only"
-                    <?php if (isset($truck_type) && $truck_type == "Power Only") echo 'selected'; ?>>Power Only
-                </option>
-                <option value="Flat Bed" <?php if (isset($truck_type) && $truck_type == "Flat Bed") echo 'selected'; ?>>
-                    Flat Bed</option>
-                <option value="Hot Shot" <?php if (isset($truck_type) && $truck_type == "Hot Shot") echo 'selected'; ?>>
-                    Hot Shot</option>
-            </select>
-        </div>
-    </div>
-    <div class="col-12 col-lg-6 panel-content-secondary">
-        <div class="col-12 col-lg-6">Truck Number :</div>
-        <div class="col-12 col-lg-6"><input type="number" class="form-control w-100" name="truck_no"
-                value="<?php if (isset($truck_no)){echo $truck_no;} ?>" /></div>
-    </div>
-    <div class="col-12 col-lg-6 panel-content-secondary">
-        <div class="col-12 col-lg-6">Trailer Number : </div>
-        <div class="col-12 col-lg-6"><input type="number" class="form-control w-100" name="trailer_no"
-                value="<?php if (isset($trailer_no)){echo $trailer_no;}  ?>"></div>
-    </div>
-    <div class="col-12 col-lg-6 panel-content-secondary">
-        <div class="col-12 col-lg-6">Truck Length (ft) :</div>
-        <div class="col-12 col-lg-6"><input type="number" class="form-control w-100" name="t_length"
-                value="<?php if (isset($t_length)){echo $t_length;} ?>" /></div>
-    </div>
-    <div class="col-12 col-lg-6 panel-content-secondary">
-        <div class="col-12 col-lg-6">Weight Limit (lbs) : </div>
-        <div class="col-12 col-lg-6"><input type="number" class="form-control w-100" name="t_weight"
-                value="<?php if (isset($t_weight)){echo $t_weight;}  ?>"></div>
+    <div class="col-lg-6 panel-content-secondary">
+        <label for="d_number">Driver Number:</label>
+        <input type="tel" id="d_number" pattern="[0-9]{10}" minlength="10" maxlength="10" 
+               class="form-control" name="d_number" value="<?php if (isset($d_number)){echo $d_number;} ?>">
     </div>
 
+    <div class="col-lg-6 panel-content-secondary">
+        <label for="truck_type">Truck Type: *</label>
+        <select name="truck_type" id="truck_type" class="form-control" required>
+            <option value="">Select truck type</option>
+            <option value="Box Truck" <?php if (isset($truck_type) && $truck_type == "Box Truck") echo 'selected'; ?>>
+                Box Truck</option>
+            <option value="Dry Van" <?php if (isset($truck_type) && $truck_type == "Dry Van") echo 'selected'; ?>>
+                Dry Van</option>
+            <option value="Reefer" <?php if (isset($truck_type) && $truck_type == "Reefer") echo 'selected'; ?>>
+                Reefer</option>
+            <option value="Power Only" <?php if (isset($truck_type) && $truck_type == "Power Only") echo 'selected'; ?>>
+                Power Only</option>
+            <option value="Flat Bed" <?php if (isset($truck_type) && $truck_type == "Flat Bed") echo 'selected'; ?>>
+                Flat Bed</option>
+            <option value="Hot Shot" <?php if (isset($truck_type) && $truck_type == "Hot Shot") echo 'selected'; ?>>
+                Hot Shot</option>
+        </select>
+    </div>
+
+    <div class="col-lg-6 panel-content-secondary">
+        <label for="truck_no">Truck Number:</label>
+        <input type="number" id="truck_no" class="form-control" name="truck_no" 
+               value="<?php if (isset($truck_no)){echo $truck_no;} ?>">
+    </div>
+
+    <div class="col-lg-6 panel-content-secondary">
+        <label for="trailer_no">Trailer Number:</label>
+        <input type="number" id="trailer_no" class="form-control" name="trailer_no" 
+               value="<?php if (isset($trailer_no)){echo $trailer_no;} ?>">
+    </div>
+
+    <div class="col-lg-6 panel-content-secondary">
+        <label for="t_length">Truck Length (ft):</label>
+        <input type="number" id="t_length" class="form-control" name="t_length" 
+               value="<?php if (isset($t_length)){echo $t_length;} ?>">
+    </div>
+
+    <div class="col-lg-6 panel-content-secondary">
+        <label for="t_weight">Weight Limit (lbs):</label>
+        <input type="number" id="t_weight" class="form-control" name="t_weight" 
+               value="<?php if (isset($t_weight)){echo $t_weight;} ?>">
+    </div>
+
+    <!-- Checkboxes with inline display -->
     <div class="col-lg-12 panel-content-secondary">
-        <div class="col-lg-3">
-            <div class="form-check form-check-inline">
-                <label class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" name="hazmat"
-                        <?php if (isset($hazmat) && $hazmat != 0) echo 'checked'; ?>>
-                    <span class="custom-control-label ml-2" for="hazmat">HAZMAT</span>
-                </label>
-            </div>
+        <div class="form-check form-check-inline">
+            <input type="checkbox" id="hazmat" class="custom-control-input" name="hazmat" 
+                   <?php if (isset($hazmat) && $hazmat != 0) echo 'checked'; ?>>
+            <label class="custom-control-label" for="hazmat">HAZMAT</label>
         </div>
-        <div class="col-lg-3">
-            <div class="form-check form-check-inline">
-                <label class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" name="twic"
-                        <?php if (isset($twic) && $twic != 0) echo 'checked'; ?>>
-                    <span class="custom-control-label ml-2" for="twic">TWIC</span>
-                </label>
-            </div>
+        <div class="form-check form-check-inline">
+            <input type="checkbox" id="twic" class="custom-control-input" name="twic" 
+                   <?php if (isset($twic) && $twic != 0) echo 'checked'; ?>>
+            <label class="custom-control-label" for="twic">TWIC</label>
         </div>
-        <div class="col-lg-3">
-            <div class="form-check form-check-inline">
-                <label class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" name="sida"
-                        <?php if (isset($sida) && $sida != 0) echo 'checked'; ?>>
-                    <span class="custom-control-label ml-2" for="sida">SIDA</span>
-                </label>
-            </div>
+        <div class="form-check form-check-inline">
+            <input type="checkbox" id="sida" class="custom-control-input" name="sida" 
+                   <?php if (isset($sida) && $sida != 0) echo 'checked'; ?>>
+            <label class="custom-control-label" for="sida">SIDA</label>
         </div>
-        <div class="col-lg-3">
-            <div class="form-check form-check-inline">
-                <label class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" name="atp"
-                        <?php if (isset($atp) && $atp != 0) echo 'checked'; ?>>
-                    <span class="custom-control-label ml-2" for="atp">Alcohol Transport Permit </span>
-                </label>
-            </div>
+        <div class="form-check form-check-inline">
+            <input type="checkbox" id="atp" class="custom-control-input" name="atp" 
+                   <?php if (isset($atp) && $atp != 0) echo 'checked'; ?>>
+            <label class="custom-control-label" for="atp">Alcohol Transport Permit</label>
         </div>
     </div>
 
-</div>
-<!-- Last line -->
-<div class="row panel-content-primary card">
-    <div class=" col-12 text-center mt-2"><button type="submit" name="submit" class="btn btn-primary" style="width:100%"
-            onclick="validateForm()">Save
-            Truck Info</button>
+    <!-- Submit button -->
+    <div class="col-12 text-center">
+        <button type="submit" name="submit" class="btn btn-primary" onclick="validateForm()">Save Truck Info</button>
     </div>
 </div>
