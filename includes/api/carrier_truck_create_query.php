82fe1014042453f431747b971bed92e01e75e772
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-09-27 01:09:18 +0500

    dispatch board 1.3.10.2 : adding driver

--- includes/api/carrier_truck_create_query.php
+++ includes/api/carrier_truck_create_query.php
@@ -13,7 +13,7 @@
       // Perform the insertion query
 
       $sql = "INSERT INTO trucks_info (company_id, carrier_id, truck_type, d_name, d_number, t_length, t_weight, truck_no, trailer_no, hazmat, twic, sida, atp)
-      VALUES ('$company_id',''$carrier_id','$truck_type', '$d_name', '$d_number', '$t_length', '$t_weight','$truck_no', '$trailer_no', '$hazmat', '$twic', '$sida', '$atp')";
+      VALUES ('$company_id','$carrier_id','$truck_type', '$d_name', '$d_number', '$t_length', '$t_weight','$truck_no', '$trailer_no', '$hazmat', '$twic', '$sida', '$atp')";
       $result = mysqli_query($connection, $sql);
       if ($result) {
         $_SESSION["message"] = "New Carrier Truck Added.";
