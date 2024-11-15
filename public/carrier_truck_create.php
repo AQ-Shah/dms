1a05c226455ce79f7750b0791fa21b9277c4b3db
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-11-15 07:37:43 +0500

    dispatch board 1.5.2.2 : editing truck debugging

--- public/carrier_truck_create.php
+++ public/carrier_truck_create.php
@@ -7,8 +7,7 @@
     if (isset($_POST['submit'])) {
       echo "<script>console.log('This is a message from PHP');</script>";
 
-      include("../includes/api/carrier_truck_create_query.php"); 
-      echo "<script>console.log(" . json_encode($current_page) .'received error' ");</script>";
+      //include("../includes/api/carrier_truck_create_query.php"); 
 
     } else {
         $_SESSION["message"] = "Please use proper form to update.";
