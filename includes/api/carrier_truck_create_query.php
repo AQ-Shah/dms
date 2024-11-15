38f23516a5d6695010aaa728657e60d083cc4dde
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-11-15 07:49:45 +0500

    dispatch board 1.5.3 : editing truck debugging

--- includes/api/carrier_truck_create_query.php
+++ includes/api/carrier_truck_create_query.php
@@ -3,27 +3,16 @@
 $prev_url = isset($_POST['prev_url']) ? $_POST['prev_url'] : 'home';
 
 // validations
-echo "<script>console.log('Entered the creation');</script>";
-
 $required_fields = array("truck_type", "d_name", "d_number", "carrier-id-for-add-truck");
 validate_presences($required_fields);
-
-echo "<script>console.log('required fields checked');</script>";
-
 include("validators/carrier_truck_post_checker.php");
-
-echo "<script>console.log('validator cleared');</script>";
 $company_id = $user['company_id'];
 
-echo "<script>console.log('Company id with carrier Id assigned for checking');</script>";
 if ($carrier_id != $company_id) { 
   $_SESSION["message"] = "Something went wrong.";
-  echo $carrier_id.' and compny id =';
-  echo $company_id;
-  //redirect_to("home");
+  redirect_to("home");
 } 
 
-echo "<script>console.log('ready for query');</script>";
 if (empty($errors)) {
   // Perform the insertion query
 
--- includes/api/carrier_truck_edit_query.php
+++ includes/api/carrier_truck_edit_query.php
@@ -6,15 +6,12 @@ $prev_url = isset($_POST['prev_url']) ? $_POST['prev_url'] : 'home';
 $required_fields = array("truck_type", "d_name", "d_number", "carrier-id-for-add-truck");
 validate_presences($required_fields);
 include("validators/carrier_truck_post_checker.php");
-
-$carrier = find_carrier_by_id($carrier_id);
 $company_id = $user['company_id'];
 
-if ($carrier['company_id'] != $company_id) { 
+if ($carrier_id != $company_id) { 
   $_SESSION["message"] = "Something went wrong.";
   redirect_to("home");
 } 
-
 
 if (empty($errors)) {
   // Perform the insertion query
@@ -33,18 +30,18 @@ if (empty($errors)) {
     twic='$twic',
     sida='$sida',
     atp='$atp' WHERE id='$carrier_id'";
-    
+
   $result = mysqli_query($connection, $sql);
 
   if ($result) {
     $_SESSION["message"] = "Carrier's Truck Updated.";
-    if ($user['permission'] == 9) {
-      redirect_to('list_sales_team_carriers');
-    }
-    redirect_to("list_carriers");
+    header("Location: " . $prev_url);
+    exit;
   } else {
     // Failure
     $_SESSION["message"] = "Carrier Update Failed : Please contact system Admin.";
+    header("Location: " . $prev_url);
+    exit;
   }
 }
 ?>
