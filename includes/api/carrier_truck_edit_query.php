91ff45f65f2f4381b7d2ec765ca47985de083aa8
Syed Abdul Qadeer <61377900+AQ-Shah@users.noreply.github.com>
2024-11-15 08:37:30 +0500

    dispatch board 1.5.2.14 : editing truck debugging

--- includes/api/carrier_truck_edit_query.php
+++ includes/api/carrier_truck_edit_query.php
@@ -11,7 +11,7 @@ $current_carrier = find_carrier_form_by_id($cid_truck_edit);
 
 if ($current_carrier['company_id'] != $company_id) { 
   $_SESSION["message"] = "Something went wrong.";
-  $current_carrier['company_id'] .'carrier cid vs usercid='. $company_id
+  $current_carrier['company_id'] .'carrier cid vs usercid='. $company_id;
   //redirect_to("home");
 } 
 
